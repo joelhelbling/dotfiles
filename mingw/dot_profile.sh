90331dd43092fa5af741e5daaaf8d7e31a8c6483
Joel Helbling <joel@joelhelbling.com>
2011-02-03 14:30:26 -0500

    added git dirty status to console prompt

--- mingw/dot_profile.sh
+++ mingw/dot_profile.sh
@@ -1,11 +1,11 @@
 
 # source from ~/.profile
 
-source ~/dotfiles/git/git_aliases.sh
-source ~/dotfiles/git/git-completion.bash
 source ~/dotfiles/file_aliases.sh
-source ~/dotfiles/mingw/ruby_switcher.sh
+source ~/dotfiles/git/git_aliases.sh
+source ~/dotfiles/ps1.sh
 source ~/dotfiles/nw_proxy.sh
+source ~/dotfiles/mingw/ruby_switcher.sh
 
 alias ruby192='use_ruby ruby 1.9.2'
 alias jruby152='use_ruby jruby 1.5.2'
@@ -15,4 +15,3 @@ alias jruby160='use_ruby jruby 1.6.0'
 
 ruby192
 
-export PS1="\e[0;33m[ \e[1;33m\w \e[0;33m$(__git_ps1)]$\e[m#=> "
