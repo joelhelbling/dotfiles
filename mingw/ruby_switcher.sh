0eb80385fc981c1aa93e3e74ee2ea035ec02d17a
Joel Helbling <joel@joelhelbling.com>
2011-02-03 16:50:16 -0500

    refactored ruby_switcher for code beauty and also fixed a couple of annoying glitches with the way it was handling path modification

--- mingw/ruby_switcher.sh
+++ mingw/ruby_switcher.sh
@@ -1,15 +1,49 @@
 
+# =================================================================================
 # Switching Rubies
+#   MinGW is kinda funny when it comes to managing multiple Rubies.  I wasn't
+#   able to get either pik (for Windows) or rvm to work in the MinGW environment.
+#
+#   I wasn't able to get pik or rvm working in Cygwin either.  And while I haven't
+#   tested this module with Cygwin, as a pure shell script solution, I am hopeful
+#   that it will work.  Just haven't had a chance to test it yet.
+#
+# =================================================================================
 
-# need ruby to appear somewhere in the path in order 
-# for this to work the first time
+# needed so it'll work the first time
 export PATH=/dummy/ruby/placeholder:$PATH
 
 use_ruby() {
   export RUBY_TYPE=$1
   export RUBY_VERSION=$2
-  NEXT_RUBY=$(ls -d1 /c/*uby* | grep -i $RUBY_TYPE | grep -i $RUBY_VERSION | sed 's:/$::' )
-  export PATH=$(echo $PATH | tr ":" "\n" | sed "s:.*ruby.*:$NEXT_RUBY/bin:i" | tr "\n" ":")
+
+  # find the desired version of ruby and trim
+  # the trailing forward slash
+  NEXT_RUBY=$(                 \
+        ls -d1 /c/*uby*        \
+      | grep -i $RUBY_TYPE     \
+      | grep -i $RUBY_VERSION  \
+      | sed 's:/$::'           )
+
+  # remove old ruby from --and add the desired
+  # ruby to-- the path
+  NEW_PATH=$NEXT_RUBY/bin:$(   \
+        echo $PATH             \
+      | tr ":" "\n"            \
+      | sed "s:.*ruby.*::i"    \
+      | tr "\n" ":"            \
+      | sed 's/:*$//'          )
+
+  # remove any duplicate colons that might've
+  # gotten in
+  export PATH=$(               \
+        echo $NEW_PATH         \
+      | sed 's/::/:/g'         )
+
+  # some nice environment variables you might
+  # want to use.  Note that could be used for
+  # scripts which need run whether we're using
+  # ruby or jruby.
   if [ "$RUBY_TYPE" == "ruby" ]; then
     export JRUBY_HOME=
     export RUBY_HOME=$NEXT_RUBY
@@ -21,7 +55,12 @@ use_ruby() {
     export RUBY_EXEC="jruby.exe"
     export GEM_EXEC="$RUBY_EXEC -S jgem"
   fi
+
+  # I hate having to remember whether to type
+  # gem or jgem
   alias gem='$GEM_EXEC'
   alias jgem='$GEM_EXEC'
+
+  # gimme some kinda sign!
   $RUBY_EXEC -v
 }
