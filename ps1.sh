11eb8d418dbd45e96292a75f8a35c674049eed41
Joel Helbling <joel@joelhelbling.com>
2011-02-03 14:37:20 -0500

    minor PS1 tweaks

--- ps1.sh
+++ ps1.sh
@@ -4,12 +4,12 @@ export CYAN="\e[1;36m"
 export BR_YELLOW="\e[1;33m"
 export DK_YELLOW="\e[0;33m"
 export DK_GREEN="\e[0;32m"
-export NORMAL_COLOR="\e[m"
+export NORMAL_COLOR='\e[m'
 
 # git completion
 export GIT_PS1_SHOWDIRTYSTATE=true
 source ~/dotfiles/git/git-completion.bash
 
 # setting the console prompt
-export PS1=' \e[1;33m\w$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s> $CYAN::$NORMAL_COLOR") '
+export PS1=' \e[1;33m\w\e[m$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s>") \e[1;36m::\e[m '
 
