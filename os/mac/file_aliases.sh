191499df34b5bfa6cf4a35e94e8ef647e139febc
Joel Helbling <joel.helbling@leandog.com>
2011-02-20 23:13:24 -0500

    added aliases for mac to show/hide hidden files in finder

--- os/mac/file_aliases.sh
+++ os/mac/file_aliases.sh
@@ -5,3 +5,8 @@ alias lS='ls -FalSG'
 
 # old habits die hard!  Just give up!
 alias gvim='mvim'
+
+# once in a while I want to see hidden files in the finder
+# (e.g. when I want to restore one using time machine)
+alias finder_show_hidden='defaults write com.apple.Finder AppleShowAllFiles YES; killall Finder'
+alias finder_hide_hidden='defaults write com.apple.Finder AppleShowAllFiles NO; killall Finder'
