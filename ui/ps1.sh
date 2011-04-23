af3e65ccef2eddfb9dee9936fffefa4ec18d53e8
Joel Helbling <joel.helbling@leandog.com>
2011-04-23 15:15:50 -0400

    switching PS1 back to old form (works better than multi-line with pushd stack)

--- ui/ps1.sh
+++ ui/ps1.sh
@@ -31,6 +31,6 @@ function format_dirs {
   fi
 }
 # setting the console prompt
-# PS1=' \e[1;33m\w\e[m$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s>$NORMAL_COLOR") \e[1;36m::\e[m '
-export PS1='\n\n\e[0;36m########\n##$(format_dirs)\n\e[0;36m##   \e[1;33m\w\e[m$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s>$NORMAL_COLOR")\n\e[0;36m##\n\e[1;36m>>>\e[m '
+export PS1=' \e[1;33m\w\e[m$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s>$NORMAL_COLOR") \e[1;36m::\e[m '
+#export PS1='\n\n\e[0;36m########\n##$(format_dirs)\n\e[0;36m##   \e[1;33m\w\e[m$(type __git_ps1 &>/dev/null && __git_ps1 "$DK_GREEN<%s>$NORMAL_COLOR")\n\e[0;36m##\n\e[1;36m>>>\e[m '
 
