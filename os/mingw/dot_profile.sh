c0f3ce949097b8b51bd2a02a1bfe37b1f488aedf
unknown <HELBINJ@NW976062.nwie.net>
2012-02-07 11:11:39 -0500

    new machine, fewer (and newer) rubies

--- os/mingw/dot_profile.sh
+++ os/mingw/dot_profile.sh
@@ -24,15 +24,8 @@ if [ "$MSYSTEM" == "MINGW32" ]; then
   source ~/dotfiles/os/mingw/ruby_switcher.sh
 
   # setup our ruby aliases
-  alias ruby192='use_ruby ruby 1.9.2'
-  alias jruby152='use_ruby jruby 1.5.2'
-  alias jruby153='use_ruby jruby 1.5.3'
-  alias jruby155='use_ruby jruby 1.5.5'
-  alias jruby160='use_ruby jruby 1.6.0.RC1'
-  alias jruby160RC2='use_ruby jruby 1.6.0.RC2'
-  alias jruby161='use_ruby jruby 1.6.1'
-  alias jruby162='use_ruby jruby 1.6.2'
-  alias jruby163='use_ruby jruby 1.6.3'
+  alias ruby193='use_ruby ruby 193'
+  alias jruby166='use_ruby jruby 166'
 
 fi
 
@@ -46,29 +39,12 @@ alias web='webopen `pwd`'
 # I don't know why putting this in a different
 # block makes it work, but it does, so I do.
 if [ "$MSYSTEM" == "MINGW32" ]; then
-  # we'll start with jruby 1.6.3
-  jruby163
+  # we'll start with ruby 1.9.3
+  ruby193
 fi
 
 export RUBYOPT='-rubygems'
 export PATH=$PATH:/c/ruby_devkit/bin
-
-#export PATH=$( echo $NEW_PATH \
-#             | sed 's!:/c/usr/local/wbin!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program\ Files/Lotus/Notes!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/ThinkPad/ConnectUtilities!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/CA/SharedComponents/PEC/bin!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/ATI Technologies/ATI.ACE/Core-Static!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/Common Files/Lenovo!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/CA/AllFusion Harvest Change Manager!!g' )
-export PATH=$( echo $NEW_PATH \
-             | sed 's!:/c/Program Files/CA/Cryptography/!!g' )
 
 export CUCUMBER_OUTPUT_ENCODING=cp1252
 alias rvm-prompt='echo $ACTIVE_RUBY'
