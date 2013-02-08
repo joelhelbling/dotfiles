0deb51800a35acaede85c7dbf80fd2c6f65e16ce
Joel Helbling <joel@joelhelbling.com>
2013-02-07 19:52:33 -0500

    removing vim related stuff (see github.com/joelhelbling/dot_vim)

--- make_dots.sh
+++ make_dots.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-function make_vimrc {
-  TARGET=$1
-  VIMFILES=$2
-
-  echo "TARGET: $TARGET"
-  echo "VIMFILES: $VIMFILES"
-  # .vimrc
-  echo "
-\" fix ctags...
-let Tlist_Ctags_Cmd = 'c:\\usr\\local\\lib\\ctags58\\ctags.exe'
-
-\" load akitaonrails vimrc
-source ~/$VIMFILES/vimrc
-
-\" load my custom vim settings
-source ~/dotfiles/vim/vimrc" > $TARGET
-
-}
-
 function make_win_profile {
   echo "
 case \`uname\` in
@@ -47,9 +28,6 @@ case `uname` in
   # .profile
   echo "source ~/dotfiles/os/mac/dot_bash_profile.sh" > ~/.bash_profile
 
-  # .vimrc
-  make_vimrc '../.vimrc' '.vim'
-
   # .gitignore
   if [ -f ~/.gitignore ]; then
     rm -f ~/.gitignore
@@ -63,9 +41,6 @@ case `uname` in
   #echo "source ~/dotfiles/os/mingw/dot_profile.sh" > ~/.profile
   make_win_profile
 
-  # _vimrc
-  make_vimrc '../_vimrc' 'vimfiles'
-
   # .gitignore
   cp git/gitignore ../.gitignore
 ;;
@@ -75,9 +50,6 @@ case `uname` in
   #echo "source ~/dotfiles/os/mingw/dot_profile.sh" > ~/.profile
   make_win_profile
 
-  # _vimrc
-  make_vimrc '../_vimrc' 'vimfiles'
-
   # .gitignore
   cp git/gitignore ../.gitignore
 ;;
@@ -86,9 +58,6 @@ case `uname` in
 
   # .profile
   sed -i 's/source ~\/dotfiles.*/source ~\/dotfiles\/os\/ubuntu\/dot_profile\.sh/g' ~/.profile
-
-  # .vimrc
-  make_vimrc '../.vimrc' '.vim'
 
   # .gitignore
   if [ -f ~/.gitignore ]; then
