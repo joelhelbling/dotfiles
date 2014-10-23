f464b2efe60d380955e664df277c6ac27bdfd5c6
Joel Helbling <joel@joelhelbling.com>
2014-10-23 19:42:31 -0400

    source nvm, add ubuntu file aliases

--- os/ubuntu/dot_bash_profile.sh
+++ os/ubuntu/dot_bash_profile.sh
@@ -2,8 +2,11 @@ export EDITOR=vim
 
 # load up the stuff we use
 source ~/dotfiles/ui/ps1.sh
+source ~/dotfiles/os/ubuntu/file_aliases.sh
 source ~/dotfiles/git/git_aliases.sh
 
 alias mvim="UBUNTU_MENUPROXY= gvim"
 
 eval 'dircolors ~/dotfiles/os/ubuntu/dircolors.ansi-dark' > /dev/null
+
+source ~/.nvm/nvm.sh
---
+++ os/ubuntu/file_aliases.sh
@@ -0,0 +1,3 @@
+# ruby/rails
+alias be='bundle exec'
+alias bers='be rspec --drb'
