4329eab353f655d95901f9e695ee30b46965ef49
Joel Helbling <joel@joelhelbling.com>
2016-08-19 10:16:57 -0400

    removing old client-focused aliases

--- os/mac/file_aliases.sh
+++ os/mac/file_aliases.sh
@@ -3,8 +3,6 @@ alias ls='ls -hFG'
 alias dir='ls -lhFGT'
 alias lS='ls -FalSG'
 
-alias email='pushd ~/vm_shared/grid && ruby lib/login.rb ff && popd'
-
 # once in a while I want to see hidden files in the finder
 # (e.g. when I want to restore one using time machine)
 alias finder_show_hidden='defaults write com.apple.Finder AppleShowAllFiles YES; killall Finder'
@@ -26,12 +24,7 @@ alias be='n bundle exec'
 
 alias rdbm='rake db:migrate db:test:prepare'
 alias bers='be rspec --drb'
-alias spork2='be spork --port 9090'
 alias bers2='RSPEC_DRB=9090 be rspec --drb'
-
-#  comverge
-alias zars='NO_BIS=y LOCALE="en-ZA" be rails s'
-alias zarc='NO_BIS=y LOCALE="en-ZA" be rails c'
 
 alias mysql_server='/usr/local/bin/mysql.server'
 
