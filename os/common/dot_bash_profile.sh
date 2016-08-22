e8cb2ab301005b94f8dcdbe157934c2aab1e71bf
Joel Helbling <joel@joelhelbling.com>
2016-08-22 10:40:42 -0400

    fixing sourcing of private files

--- os/common/dot_bash_profile.sh
+++ os/common/dot_bash_profile.sh
@@ -3,27 +3,25 @@ HISTCONTROL=ignoredups
 
 export EDITOR=vim
 
-# load up the stuff we use
 source ~/dotfiles/ui/ps1.sh
 source ~/dotfiles/ui/file_aliases.sh
 source ~/dotfiles/git/git_aliases.sh
 
+echo "HERE WE GO (verbose: $VERBOSE)"
+
 # source private (read "custom") scripts
-if ls ~/dotfiles/private/* 1> /dev/null 2>&1; then
+if [ $VERBOSE -ge 1 ]; then
+  echo "sourcing private files..."
+fi
+
+for file in ~/dotfiles/private/*.sh; do
   if [ $VERBOSE -ge 1 ]; then
-    echo "sourcing private files..."
+    echo " - $file"
   fi
+  source $file
+done
 
-  for file in ~/dotfiles/private/*; do
-    source $file
-
-    if [ $VERBOSE -ge 1 ]; then
-      echo " - $file"
-    fi
-  done
-
-  if [ $VERBOSE -ge 1 ]; then
-    echo "All files sourced."
-  fi
+if [ $VERBOSE -ge 1 ]; then
+  echo "All files sourced."
 fi
 
