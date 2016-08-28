ad86c9a888c0e0f7d5895ce46707ffb33758c3be
Joel Helbling <joel@joelhelbling.com>
2016-08-27 20:40:47 -0400

    fixing verbose mode

--- os/common/dot_bash_profile.sh
+++ os/common/dot_bash_profile.sh
@@ -10,18 +10,18 @@ source ~/dotfiles/git/git_aliases.sh
 # source private (read "custom") scripts
 sourceScriptsInDir () {
   sourceDir=$1
-  if [ $VERBOSE -ge 1 ]; then
+  if [ "$VERBOSE" == "1" ]; then
     echo "sourcing *.sh in $sourceDir..."
   fi
 
   for file in $sourceDir/*.sh; do
-    if [ $VERBOSE -ge 1 ]; then
+    if [ "$VERBOSE" == "1" ]; then
       echo " - $file"
     fi
     source $file
   done
 
-  if [ $VERBOSE -ge 1 ]; then
+  if [ "$VERBOSE" == "1" ]; then
     echo "All files sourced."
   fi
 }
