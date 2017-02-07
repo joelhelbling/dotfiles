5f9a421edc83c49919c497a883cba0b52ee3439c
Joel Helbling <joel@joelhelbling.com>
2017-02-07 22:26:46 +0000

    adds-to-path and inits rbenv

--- custom/rbenv_setup.sh
+++ custom/rbenv_setup.sh
@@ -1,3 +1,8 @@
+if [ -d ~/.rbenv/bin ] && [ -z `which rbenv` ]; then
+  export PATH=~/.rbenv/bin:$PATH
+  eval "$(rbenv init -)"
+fi
+
 if [ ! -z `which rbenv` ] && [ -d ~/.rbenv/shims ]; then
   export PATH=~/.rbenv/shims:$PATH
 fi
