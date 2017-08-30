aaab7b2024ea45b06cd2a24bb32346b291e0fc2a
Joel Helbling <joel@joelhelbling.com>
2017-08-30 00:55:26 -0700

    major changes to PS1 (untested in Ubuntu, msys)

--- ui/ps1.sh
+++ ui/ps1.sh
@@ -1,24 +1,56 @@
 #!/bin/sh
 
 # colors
-export RED="\[\e[1;31m\]"
-export BLUE="\[\e[0;34m\]"
-export LIGHT_CYAN="\[\e[0;36m\]"
-export BR_YELLOW="\[\e[1;33m\]"
-export DK_YELLOW="\[\e[0;33m\]"
-export DK_GREEN="\[\e[0;32m\]"
-export LIGHT_GREEN="\[\e[1;32m\]"
-export PERIWINKLE="\[\e[1;35m\]"
-export MAGENTA="\[\e[0;35m\]"
-export NORMAL_COLOR="\[\e[m\]"
+# export RED="\[\e[1;31m\]"
+# export BLUE="\[\e[0;34m\]"
+# export LIGHT_CYAN="\[\e[0;36m\]"
+# export BR_YELLOW="\[\e[1;33m\]"
+# export DK_YELLOW="\[\e[0;33m\]"
+# export DK_GREEN="\[\e[0;32m\]"
+# export LIGHT_GREEN="\[\e[1;32m\]"
+# export PERIWINKLE="\[\e[1;35m\]"
+# export MAGENTA="\[\e[0;35m\]"
 
-export   CLOSE="${NORMAL_COLOR}▶"
-export  PROMPT="${DK_YELLOW}✝${NORMAL_COLOR} "
-export EXTRA_SPACE=""
+export BOLD_ON="\[\e[1m\]"
+export BOLD_OFF="\[\e[22m\]"
+export INVERSE_ON="\[\e[7m\]"
+export INVERSE_OFF="\[\e[27m\]"
+export ITALICS_ON="\[\e[3m\]"
+export ITALICS_OFF="\[\e[23m\]"
+export STRIKETHROUGH_ON="\[\e[9m\]"
+export STRIKETHROUGH_OFF="\[\e[29m\]"
+export UNDERLINE_ON="\[\e[4m\]"
+export UNDERLINE_OFF="\[\e[24m\]"
 
-if [[ "$(uname)" = "Linux" ]]; then
-  export EXTRA_SPACE=" "
-fi
+export FG_BLACK="\[\e[30m\]"
+export FG_BLUE="\[\e[34m\]"
+export FG_CYAN="\[\e[36m\]"
+export FG_DEFAULT="\[\e[39m\]"
+export FG_GREEN="\[\e[32m\]"
+export FG_MAGENTA="\[\e[35m\]"
+export FG_RED="\[\e[31m\]"
+export FG_YELLOW="\[\e[33m\]"
+export FG_WHITE="\[\e[37m\]"
+
+export BG_BLACK="\[\e[40m\]"
+export BG_BLUE="\[\e[44m\]"
+export BG_CYAN="\[\e[46m\]"
+export BG_DEFAULT="\[\e[49m\]"
+export BG_GREEN="\[\e[42m\]"
+export BG_MAGENTA="\]\e[45m\]"
+export BG_RED="\[\e[41m\]"
+export BG_YELLOW="\[\e[43m\]"
+export BG_WHITE="\[\e[47m\]"
+
+export COLOR_RESET="\[\e[0m\]" # reset; clears all colors and styles (to white on black)
+
+export DIVIDER="◤"
+export PROMPT="►"
+
+# if [[ "$(uname)" = "Linux" ]]; then
+#   export EXTRA_SPACE=" "
+# fi
+export EXTRA_SPACE=" "
 
 # show * for unstaged and + for uncommitted changes
 export GIT_PS1_SHOWDIRTYSTATE=true
@@ -54,34 +86,44 @@ function format_dirs {
 
 function rbenv_version {
   if [ ! -z `which rbenv` ]; then
-    echo "ruby-`rbenv local 2> /dev/null || rbenv global 2> /dev/null || echo '???'` ▶"
+    echo "ruby-`rbenv local 2> /dev/null || rbenv global 2> /dev/null || echo '???'` "
   fi
 }
 
 function node_version {
   if [ -d ~/.nvm ] && [ ! -z "$(type nvm 2> /dev/null)" ]; then
-    echo "node-`nvm current | sed 's/v//'` ▶"
+    echo "node-`nvm current | sed 's/v//'`"
   elif [ ! -z `which nodenv` ]; then
-    echo "node-`nodenv local 2> /dev/null || nodenv global 2> /dev/null || echo '???'` ▶"
+    echo "node-`nodenv local 2> /dev/null || nodenv global 2> /dev/null || echo '???'` "
   fi
 }
 
 function git_prompt {
-  type __git_ps1 &>/dev/null && __git_ps1 "\e[1;35m⎇  %s ▶"
+  type __git_ps1 &>/dev/null && __git_ps1 "\e[37m⎇  %s "
+}
+
+function time_stamp {
+  echo `date "+%Y.%m.%d %H:%M:%S %Z"`
 }
 
 # setting the console prompt
 export PS1="\
-$LIGHT_CYAN\
+$ITALICS_ON${FG_BLUE}\
+...completed at $(echo '$(time_stamp)') ⇪\
+$ITALICS_OFF\n\n\
+$BG_BLUE$FG_WHITE \u@\h \
+$BG_RED$FG_BLUE$DIVIDER\
+$BG_RED$FG_WHITE$EXTRA_SPACE\
+$(echo '$(rbenv_version)')\
+$BG_GREEN$FG_RED$DIVIDER\
+$BG_GREEN$FG_WHITE$EXTRA_SPACE$(echo '$(node_version)')\
+$BG_CYAN$FG_GREEN$DIVIDER\
+$BG_CYAN$FG_WHITE$EXTRA_SPACE$(echo '$(git_prompt)')\
+$BG_DEFAULT$FG_CYAN$DIVIDER\
+$FG_DEFAULT\
 $(echo '$(format_dirs)')\
-\n
-$CLOSE\
-$RED$EXTRA_SPACE$(echo '$(rbenv_version)')\
-$DK_GREEN$EXTRA_SPACE$(echo '$(node_version)')\
-$EXTRA_SPACE$(echo '$(git_prompt)')\
 \n\
-$CLOSE\
-$EXTRA_SPACE$BR_YELLOW\u@\h:\
-$LIGHT_CYAN\w/ \
-$PROMPT"
+$BG_DEFAULT$FG_BLUE[0] \w/ \
+$FG_YELLOW$PROMPT$BOLD_OFF\
+$FG_DEFAULT "
 
