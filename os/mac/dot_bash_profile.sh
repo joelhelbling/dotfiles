427fc5c1caa8cf5976e1218b8cfc1ae3dd036573
Joel Helbling <joel@joelhelbling.com>
2018-11-02 15:35:15 -0400

    privatize java setup, fix path, rubygems

--- os/mac/dot_bash_profile.sh
+++ os/mac/dot_bash_profile.sh
@@ -3,12 +3,6 @@ VERBOSE=0
 source ~/dotfiles/os/common/dot_bash_profile.sh
 source ~/dotfiles/os/mac/file_aliases.sh
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home
-export RUBYOPT='-rubygems'
 export PROJECT_DIR=$HOME/code
 
 export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
-
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-export PATH=$JAVA_HOME/bin/java:$PATH
-
