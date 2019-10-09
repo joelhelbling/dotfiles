6d3c1bc1843eda7b7e356f20b00425f2ed365ee8
Joel Helbling <joel@joelhelbling.com>
2019-10-09 14:11:18 -0400

    turns color for grep back on
    Why did I have this commented?

--- ui/file_aliases.sh
+++ ui/file_aliases.sh
@@ -12,3 +12,4 @@ alias du='du -h'
 # Misc :)
 alias less='less -r'                          # raw control characters
 alias whence='type -a'                        # where, of a sort
+alias grep='grep --color'                     # show differences in colour
