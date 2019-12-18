121e61779e17ea4bc83bd3c04fc851aa6bf620fe
Joel Helbling <joel@joelhelbling.com>
2019-12-18 15:19:25 -0500

    fixes asdf for linux

--- custom/asdf_setup.sh
+++ custom/asdf_setup.sh
@@ -1,19 +1,16 @@
-
-if [ ! -z `which asdf` ]; then
+if [ `uname` == "Darwin" ]; then
+  prefix=$(brew --prefix asdf)
+  asdf_path="$prefix/asdf.sh"
+  asdf_completions_path="$prefix/etc/bash_completion.d/asdf.bash"
+else
   asdf_path="$HOME/.asdf/asdf.sh"
   asdf_completions_path="$HOME/.asdf/completions/asdf.bash"
+fi
 
-  if [ `uname` == "Darwin" ]; then
-    prefix=$(brew --prefix asdf)
-    asdf_path="$prefix/asdf.sh"
-    asdf_completions_path="$prefix/etc/bash_completion.d/asdf.bash"
-  fi
+if [ -f $asdf_path ]; then
+  . $asdf_path
+fi
 
-  if [ -f $asdf_path ]; then
-    . $asdf_path
-  fi
-
-  if [ -f $asdf_completions_path ]; then
-    . $asdf_completions_path
-  fi
+if [ -f $asdf_completions_path ]; then
+  . $asdf_completions_path
 fi
