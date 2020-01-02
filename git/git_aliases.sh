a4baf79ba5bcbb210189338be39f66eb42307bd3
Joel Helbling <joel@joelhelbling.com>
2020-01-02 11:32:30 -0500

    adds git aliases
    - gai,gap: adds interactively
    - gbd,gdb: diff current branch back to its origin off master

--- git/git_aliases.sh
+++ git/git_aliases.sh
@@ -8,11 +8,15 @@ fi
 # Git aliases
 alias gs='git status'
 alias ga='git add'
+alias gap='git add -p'
+alias gai='git add -p'
 alias gb='git branch'
 alias gba='git branch -a'
 alias gbv='git branch -vv'
+alias gbd="git diff \`git merge-base --fork-point master\`"
 alias gc='git commit'
 alias gd='git diff'
+alias gdb="git diff \`git merge-base --fork-point master\`"
 alias gh='git hist'
 alias gco='git checkout'
 alias gull='git pull origin master'
