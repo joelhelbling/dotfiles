754a6bfa610c957bf2598e932e0c4690bab2597c
Joel Helbling <joel@joelhelbling.com>
2020-02-01 18:29:01 -0500

    Solve for Ubuntu

--- custom/asdf_setup.sh
+++ custom/asdf_setup.sh
@@ -1,10 +1,10 @@
+asdf_path="$HOME/.asdf/asdf.sh"
+asdf_completions_path="$HOME/.asdf/completions/asdf.bash"
+
 if [ `uname` == "Darwin" ]; then
   prefix=$(brew --prefix asdf)
   asdf_path="$prefix/asdf.sh"
   asdf_completions_path="$prefix/etc/bash_completion.d/asdf.bash"
-else
-  asdf_path="$HOME/.asdf/asdf.sh"
-  asdf_completions_path="$HOME/.asdf/completions/asdf.bash"
 fi
 
 if [ -f $asdf_path ]; then
