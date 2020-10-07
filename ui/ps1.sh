a80b5624dec6ab71fbc046b921b55fa0360a3d02
Joel Helbling <joel@joelhelbling.com>
2020-10-06 23:12:19 -0400

    refactored PS1 lang version coloring
    The eventual goal here is to read `.tool-versions` in the current
    directory, and only show language versions for languages defined there.
    
    This commit adds dictionaries for the foreground and background colors
    for each of two languages (the two that I've previously displayed, ruby
    and node).

--- ui/ps1.sh
+++ ui/ps1.sh
@@ -1,61 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-# colors
-# export RED="\[\e[1;31m\]"
-# export BLUE="\[\e[0;34m\]"
-# export LIGHT_CYAN="\[\e[0;36m\]"
-# export BR_YELLOW="\[\e[1;33m\]"
-# export DK_YELLOW="\[\e[0;33m\]"
-# export DK_GREEN="\[\e[0;32m\]"
-# export LIGHT_GREEN="\[\e[1;32m\]"
-# export PERIWINKLE="\[\e[1;35m\]"
-# export MAGENTA="\[\e[0;35m\]"
+BOLD_ON="\[\e[1m\]"
+BOLD_OFF="\[\e[22m\]"
+INVERSE_ON="\[\e[7m\]"
+INVERSE_OFF="\[\e[27m\]"
+ITALICS_ON="\[\e[3m\]"
+ITALICS_OFF="\[\e[23m\]"
+STRIKETHROUGH_ON="\[\e[9m\]"
+STRIKETHROUGH_OFF="\[\e[29m\]"
+UNDERLINE_ON="\[\e[4m\]"
+UNDERLINE_OFF="\[\e[24m\]"
 
-export BOLD_ON="\[\e[1m\]"
-export BOLD_OFF="\[\e[22m\]"
-export INVERSE_ON="\[\e[7m\]"
-export INVERSE_OFF="\[\e[27m\]"
-export ITALICS_ON="\[\e[3m\]"
-export ITALICS_OFF="\[\e[23m\]"
-export STRIKETHROUGH_ON="\[\e[9m\]"
-export STRIKETHROUGH_OFF="\[\e[29m\]"
-export UNDERLINE_ON="\[\e[4m\]"
-export UNDERLINE_OFF="\[\e[24m\]"
+FG_BLACK="\[\e[30m\]"
+FG_BLUE="\[\e[34m\]"
+FG_CYAN="\[\e[36m\]"
+FG_DEFAULT="\[\e[39m\]"
+FG_GREEN="\[\e[32m\]"
+FG_MAGENTA="\[\e[35m\]"
+FG_RED="\[\e[31m\]"
+FG_YELLOW="\[\e[33m\]"
+FG_WHITE="\[\e[37m\]"
 
-export FG_BLACK="\[\e[30m\]"
-export FG_BLUE="\[\e[34m\]"
-export FG_CYAN="\[\e[36m\]"
-export FG_DEFAULT="\[\e[39m\]"
-export FG_GREEN="\[\e[32m\]"
-export FG_MAGENTA="\[\e[35m\]"
-export FG_RED="\[\e[31m\]"
-export FG_YELLOW="\[\e[33m\]"
-export FG_WHITE="\[\e[37m\]"
+BG_BLACK="\[\e[40m\]"
+BG_BLUE="\[\e[44m\]"
+BG_CYAN="\[\e[46m\]"
+BG_DEFAULT="\[\e[49m\]"
+BG_GREEN="\[\e[42m\]"
+BG_MAGENTA="\]\e[45m\]"
+BG_RED="\[\e[41m\]"
+BG_YELLOW="\[\e[43m\]"
+BG_WHITE="\[\e[47m\]"
 
-export BG_BLACK="\[\e[40m\]"
-export BG_BLUE="\[\e[44m\]"
-export BG_CYAN="\[\e[46m\]"
-export BG_DEFAULT="\[\e[49m\]"
-export BG_GREEN="\[\e[42m\]"
-export BG_MAGENTA="\]\e[45m\]"
-export BG_RED="\[\e[41m\]"
-export BG_YELLOW="\[\e[43m\]"
-export BG_WHITE="\[\e[47m\]"
+COLOR_RESET="\[\e[0m\]" # reset; clears all colors and styles (to white on black)
 
-export COLOR_RESET="\[\e[0m\]" # reset; clears all colors and styles (to white on black)
+DIVIDER="▞"
+PROMPT="::"
 
-export DIVIDER="▞"
-export PROMPT="::"
-
-export EXTRA_SPACE=" "
+EXTRA_SPACE=" "
 
 # show * for unstaged and + for uncommitted changes
-export GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWDIRTYSTATE=true
 
 # show $ if there are stashed files
-export GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWSTASHSTATE=true
 
-# git
+# git bash completion
 if [[ "$(uname -s)" != "Darwin" ]] || [ -z `which brew` ] || [ ! -f $(brew --prefix)/etc/bash_completion ]; then
   source ~/dotfiles/git/git-completion.bash
   source ~/dotfiles/git/git-prompt.sh
@@ -75,11 +64,29 @@ function prettify {
 }
 
 function format_dirs {
-  echo ""
   for DER in `dir_stack`; do
     echo $(prettify $DER)
   done
 }
+
+declare -A lang_fg=(        \
+  ["default"]="$FG_DEFAULT" \
+  ["host"]="$BOLD_ON$ITALICS_ON$FG_WHITE"      \
+  ["ruby"]="$FG_WHITE"      \
+  ["nodejs"]="$FG_WHITE"    \
+)
+declare -A lang_bg=(        \
+  ["default"]="$BG_DEFAULT" \
+  ["host"]="$BG_BLUE"       \
+  ["ruby"]="$BG_RED"        \
+  ["nodejs"]="$BG_GREEN"    \
+)
+declare -A lang_fg_alt=(    \
+  ["default"]="$FG_DEFAULT" \
+  ["host"]="$FG_BLUE$BOLD_OFF$ITALICS_OFF"       \
+  ["ruby"]="$FG_RED"        \
+  ["nodejs"]="$FG_GREEN"    \
+)
 
 function ruby_version {
   if [ ! -z `which asdf` ] && [ ! -z "`asdf plugin list | grep ruby`" ]; then
@@ -89,7 +96,7 @@ function ruby_version {
   fi
 }
 
-function node_version {
+function nodejs_version {
   if [ ! -z `which asdf` ] && [ ! -z "`asdf plugin list | grep nodejs`" ]; then
     echo "nd:`asdf current nodejs | sed 's/\s\+/ /g' | cut -d ' ' -f 2` "
   elif [ ! -z `which nodenv` ]; then
@@ -99,6 +106,10 @@ function node_version {
   else
     echo 'nd: ???'
   fi
+}
+
+function default_version {
+  echo ""
 }
 
 function git_prompt {
@@ -117,20 +128,33 @@ if [ "`type -t iterm2_prompt_mark`" == "" ]; then
   }
 fi
 
+function colors_for_lang {
+  lang=$1
+  echo "${lang_bg[$lang]}${lang_fg[$lang]}$EXTRA_SPACE"
+}
+
+function transition_divider {
+  from=$1
+  tew=$2
+  echo "${lang_bg[$tew]}${lang_fg_alt[$from]}$DIVIDER"
+}
+
+function transition_lang {
+  from=$1
+  tew=$2
+  echo "$(transition_divider $from $tew)$(colors_for_lang $tew)$(${tew}_version)"
+}
+
 # setting the console prompt
 export PS1="\n\
 $ITALICS_ON$BG_DEFAULT$FG_DEFAULT\
 `echo '$(time_stamp)'`\
 $BG_DEFAULT$ITALICS_OFF\n\
-$BG_BLUE$FG_WHITE @\h \
-$BG_RED$FG_BLUE$DIVIDER\
-$BG_RED$FG_WHITE$EXTRA_SPACE\
-`echo '$(ruby_version)'`\
-$BG_GREEN$FG_RED$DIVIDER\
-$BG_GREEN$FG_WHITE$EXTRA_SPACE\
-`echo '$(node_version)'`\
-$BG_DEFAULT$FG_GREEN$DIVIDER\
-$FG_DEFAULT\
+`colors_for_lang "host"`\
+@\h \
+`transition_lang "host" "ruby"`\
+`transition_lang "ruby" "nodejs"`\
+`transition_lang "nodejs" "default"`\
 `echo '$(format_dirs)'`\
 \n\
 \[$(iterm2_prompt_mark)\]\
