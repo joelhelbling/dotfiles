61bfcd4662ceebc82ce7f4e5c92deec4c67bbd0f
Joel Helbling <joel@joelhelbling.com>
2020-10-06 21:52:59 -0400

    adds bash completion for linux

--- custom/bash_completion.sh
+++ custom/bash_completion.sh
@@ -1,3 +1,5 @@
 if [[ "$(uname -s)" = "Darwin" ]] && [ -f /usr/local/etc/bash_completion ]; then
   source /usr/local/etc/bash_completion
+elif [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]]; then
+  source /usr/share/bash-completion/bash_completion
 fi
